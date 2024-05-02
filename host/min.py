f0aba8907688684716ee5ef905dc62d660ee3ba4
James Jenkins <james.jenkins745@protonmail.com>
2024-05-02 15:24:30 -0500

    changed baudrate to 115200

--- host/min.py
+++ host/min.py
@@ -799,7 +799,7 @@ class MINTransportSerial(MINTransport):
     def _serial_close(self):
         self._serial.close()
 
-    def __init__(self, port, baudrate=11500, loglevel=ERROR):
+    def __init__(self, port, baudrate=115200, loglevel=ERROR):
         """
         Open MIN connection on a given port.
         :param port: serial port
